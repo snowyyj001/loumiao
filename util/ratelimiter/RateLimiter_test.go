4016ba0b40c4115ec0989643d1e2ac851e3353e9
魏金龙 <weijinlong@2144.cn>
2022-01-25 11:39:29 +0800

    令牌桶算法实现

---
+++ util/ratelimiter/RateLimiter_test.go
@@ -0,0 +1,79 @@
+package ratelimiter_test
+
+import (
+	"fmt"
+	"github.com/snowyyj001/loumiao/util/ratelimiter"
+	"sync"
+	"testing"
+	"time"
+)
+
+//go test -v -run Test_rl RateLimiter_test.go
+func Test_rl(t *testing.T) {
+
+	var permitsPerSecond int = 100
+	var maxPermits int = 200
+	limter := ratelimiter.Create(permitsPerSecond, maxPermits)
+
+	suc := 0
+	a1 := 0
+	a2 := 0
+
+	maxv := 600
+
+	wg := &sync.WaitGroup{}
+
+	t1 := time.Now().Unix()
+
+	wg.Add(1)
+	go func() {
+		for {
+			limter.Acquire()
+			suc++
+			a1++
+			if suc >= maxv {
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for {
+			limter.Acquire()
+			suc++
+			a2++
+			if suc >= maxv {
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for {
+			limter.Acquire()
+			suc++
+			a2++
+			if suc >= maxv {
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	t2 := time.Now().Unix()
+
+
+	//ratelimiter起步就有maxPermits可用，然后每秒产生permitsPerSecond个
+	//因此maxv个需要t2 - t1秒，忽略小数部分的毫秒
+	if int(t2 - t1) != (maxv - maxPermits) / (permitsPerSecond) {
+		t.FailNow()
+	}
+
+	fmt.Println("Test_rl done", a1, a2, suc, t2 - t1)
+}
